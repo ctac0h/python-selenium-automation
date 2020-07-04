1209dab18e36ef246feb4280f1ee468cba50e3ad
ctac0h <compizzz@gmail.com>
2020-07-03 18:38:06 -0700

    Revert "Project structure change"
    This reverts commit 723965d3cba848602b51e35432d9ddfc9def316f.

--- features/steps/orders.py
+++ features/steps/orders.py
@@ -3,7 +3,7 @@ from behave import given, when, then
 from time import sleep
 
 ORDERS_BUTTON = (By.ID, "nav-orders")
-
+EXPECTED_TITLE = "Amazon Sign-In"
 
 
 @given("Open Amazon Page")
@@ -16,3 +16,8 @@ def click_orders_button(context):
     context.driver.find_element(*ORDERS_BUTTON).click()
     sleep(1)
 
+
+@then("\"Sign in\" page opened")
+def verify_sign_in_page(context):
+    page_title = context.driver.title
+    assert page_title == EXPECTED_TITLE, f"Expected title is: {EXPECTED_TITLE}, but got {page_title}"
